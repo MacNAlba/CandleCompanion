c95e8fadd22d1dd49afc93d21e85662e79780589
macnalba <paul@mcphillips.cc>
2022-06-25 14:22:30 +0100

    Delete table entry function completed Data validation corrections made on some entry labels that couldn't handle decimal points for required floats More try except blocks added

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 import sqlite3
+from sqlite3 import Connection
 from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showinfo
-
+# TODO create an error function to handle all error messages and output them to a separate error log file
 # ==== Database Connection ====
-db = sqlite3.connect("db.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)
-db.execute("CREATE TABLE IF NOT EXISTS materials (ingredient TEXT NOT NULL, quantity REAL NOT NULL, cost REAL)")
-db.execute("CREATE TABLE IF NOT EXISTS products (product TEXT NOT NULL, scent TEXT NOT NULL, decoration TEXT, "
-           "quantity REAL NOT NULL, price REAL NOT NULL)")
-
+try:
+    db: Connection = sqlite3.connect("db.sqlite")
+    db.execute("CREATE TABLE IF NOT EXISTS materials (ingredient TEXT NOT NULL, quantity REAL NOT NULL, cost REAL)")
+    db.execute("CREATE TABLE IF NOT EXISTS products (product TEXT NOT NULL, scent TEXT NOT NULL, decoration TEXT, "
+               "quantity INTEGER NOT NULL, price REAL NOT NULL)")
+except Exception as e:
+    print("Database failed to initialise {}".format(e))
+    exit()
 
 # ==== Functions ====
-def item_selected():
-    for selected_item in tree.selection():
-        item = tree.item(selected_item)
-        record = item['values']
-        return record
-
-
 def refresh():
     for item in tree.get_children():
         tree.delete(item)
@@ -29,13 +26,15 @@ def refresh():
 def add_product():
     c = db.cursor()
     try:
-        if product_entry.get().isalpha() and len(product_entry.get()) > 0:
-            if scent_entry.get().isalpha() and len(scent_entry.get()) > 0:
-                if quantity_entry.get().isnumeric() and len(quantity_entry.get()) > 0:
-                    if price_entry.get().isnumeric() and len(quantity_entry.get()) > 0:
+        if str(product_entry.get()).isalpha() and len(product_entry.get()) > 0:
+            if str(scent_entry.get()).isalpha() and len(scent_entry.get()) > 0:
+                if int(quantity_entry.get()) and len(quantity_entry.get()) > 0:
+                    if float(price_entry.get()) and len(quantity_entry.get()) > 0:
 
-                        exists = c.execute("SELECT product FROM products WHERE (product=? and scent=? and decoration=? and price=?)",
-                                  (product_entry.get(), scent_entry.get(), decorator_entry.get(), price_entry.get())).fetchall()
+                        exists = c.execute(
+                            "SELECT product FROM products WHERE (product=? and scent=? and decoration=? and price=?)",
+                            (product_entry.get(), scent_entry.get(), decorator_entry.get(),
+                             price_entry.get())).fetchall()
                         if not exists:
                             db.execute("INSERT INTO products VALUES(?, ?, ?, ?, ?)",
                                        (str(product_entry.get()), str(scent_entry.get()),
@@ -49,7 +48,10 @@ def add_product():
                                                                    decorator_entry.get(), quantity_entry.get(),
                                                                    price_entry.get()))
                         else:
-                            fetch = c.execute("SELECT quantity FROM products WHERE (product=? and scent=? and decoration=? and price=?)", (product_entry.get(), scent_entry.get(), decorator_entry.get(),price_entry.get())).fetchone()
+                            fetch = c.execute(
+                                "SELECT quantity FROM products WHERE (product=? and scent=? and decoration=? and price=?)",
+                                (product_entry.get(), scent_entry.get(), decorator_entry.get(),
+                                 price_entry.get())).fetchone()
                             orig_qty = fetch[0]
                             add_qty = float(quantity_entry.get())
                             new_qty = orig_qty + add_qty
@@ -57,7 +59,8 @@ def add_product():
                             print(fetch)
                             update = "UPDATE products SET quantity=? WHERE product=? and scent=? and decoration=? and price=?"
 
-                            c.execute(update, (new_qty, product_entry.get(), scent_entry.get(), decorator_entry.get(),price_entry.get()))
+                            c.execute(update, (
+                            new_qty, product_entry.get(), scent_entry.get(), decorator_entry.get(), price_entry.get()))
                             c.close()
                             refresh()
 
@@ -78,11 +81,29 @@ def add_product():
         pass
 
 
-# TODO Get the delete function to actually remove items from database
+def get_selection(event):
+    curItem = tree.focus()
+    contents = (tree.item(curItem))
+    selecteddetails = contents['values']
+    product_entry.delete(0, tk.END)
+    scent_entry.delete(0, tk.END)
+    decorator_entry.delete(0, tk.END)
+    quantity_entry.delete(0, tk.END)
+    price_entry.delete(0, tk.END)
+    product_entry.insert(0, selecteddetails[0])
+    scent_entry.insert(0, selecteddetails[1])
+    decorator_entry.insert(0, selecteddetails[2])
+    quantity_entry.insert(0, selecteddetails[3])
+    price_entry.insert(0, selecteddetails[4])
+    return
+
+
 def delete_product():
     try:
-        line = tree.selection()
-        db.execute("DELETE FROM products WHERE products.product=?", (line,))
+        c = db.cursor()
+        query = "DELETE FROM products WHERE product=? and scent=? and decoration=? and quantity=? and price=?"
+        selection = (product_entry.get(), scent_entry.get(), decorator_entry.get(), quantity_entry.get(), price_entry.get())
+        c.execute(query, selection)
         message_label.config(text="Entry deleted")
         db.commit()
         refresh()
@@ -117,25 +138,26 @@ mWin.rowconfigure(6, weight=5)
 mWin.rowconfigure(7, weight=1)
 
 # ==== TreeView ====
-prod_columns = ('product', 'scent', 'decoration', 'quantity', 'price')
-tree = ttk.Treeview(mWin, columns=prod_columns, show='headings')
+try:
+    prod_columns = ('product', 'scent', 'decoration', 'quantity', 'price')
+    tree = ttk.Treeview(mWin, columns=prod_columns, show='headings')
 
-tree.heading('product', text='Products')
-tree.heading('scent', text='Scent')
-tree.heading('decoration', text='Decoration')
-tree.heading('quantity', text='Quantity')
-tree.heading('price', text='Price')
+    tree.heading('product', text='Products')
+    tree.heading('scent', text='Scent')
+    tree.heading('decoration', text='Decoration')
+    tree.heading('quantity', text='Quantity')
+    tree.heading('price', text='Price')
 
-for product in db.execute("SELECT * FROM products ORDER BY products.product"):
-    tree.insert('', tk.END, values=product)
+    for product in db.execute("SELECT * FROM products ORDER BY products.product"):
+        tree.insert('', tk.END, values=product)
+    tree.bind('<<TreeviewSelect>>', get_selection,)
+    tree.grid(row=5, column=0, sticky='nsew', rowspan=2, columnspan=4, padx=(30, 0))
 
-tree.bind('<<TreeviewSelect>>')
-
-tree.grid(row=5, column=0, sticky='nsew', rowspan=2, columnspan=4, padx=(30, 0))
-
-scrollbar = ttk.Scrollbar(mWin, orient=tk.VERTICAL, command=tree.yview)
-tree.configure(yscrollcommand=scrollbar.set)
-scrollbar.grid(row=5, column=3, sticky='nse')
+    scrollbar = ttk.Scrollbar(mWin, orient=tk.VERTICAL, command=tree.yview)
+    tree.configure(yscrollcommand=scrollbar.set)
+    scrollbar.grid(row=5, column=3, sticky='nse')
+except Exception as e:
+    print("TreeView failed to initialise {}".format(e))
 
 # ==== Frames ====
 frame = tk.Frame(mWin)
